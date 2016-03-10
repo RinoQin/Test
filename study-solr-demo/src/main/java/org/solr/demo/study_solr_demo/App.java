e95ad75b54a5843b045b495fc229ec90d453fc0a
QinYuchen <j.g.qin@163.com>
2016-03-10 15:33:07 +0800

    初建项目

---
+++ study-solr-demo/src/main/java/org/solr/demo/study_solr_demo/App.java
@@ -0,0 +1,13 @@
+package org.solr.demo.study_solr_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
